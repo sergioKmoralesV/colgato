0ebdfa2d3757de26aaa45df9bdbf5037971f0e8d
Sergio Morales <s.mora.94@gmail.com>
2015-11-25 13:17:20 -0400

    Clues completed

--- public/images/Pig_files/hue_common_header.js
+++
@@ -1,8 +0,0 @@
-
-if(window.top != window) {
-    //we are in frame
-    var l = document.location;
-    document.createElement("img").src =
-            l.protocol + "//" + l.hostname + ":8888" + "/sync/?loc=" +
-            escape(document.location.href);
-}
